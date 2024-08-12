8dc75171c88cf23c94c93200ea4812a026afc11d
ahmadmuhammadgd <ahmadmuhammadgd@gmail.com>
2024-08-12 23:42:43 +0300

    delete;

--- includes/SQL/dremio_views/view_bronz.sql
+++
@@ -1,5 +0,0 @@
-SELECT * FROM nessie.bronz_raw_customers;
-
-SELECT * FROM nessie.bronz_raw_fraud_tranactions;
-
-SELECT * FROM nessie.bronz_raw_transactions;
--- includes/SQL/dremio_views/view_silver.sql
+++
@@ -1,5 +0,0 @@
-SELECT * FROM nessie.silver_customers;
-
-SELECT * FROM nessie.silver_fraud_transactions;
-
-SELECT * FROM nessie.silver_transactions;
