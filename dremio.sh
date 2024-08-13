3f4ffda5cbc95e102f2c139ce0eb5a849fa6eef2
ahmadmuhammadgd <ahmadmuhammadgd@gmail.com>
2024-08-13 20:22:27 +0300

    setup bash scripts

--- dremio.sh
+++ dremio.sh
@@ -1,35 +1,74 @@
-    username=ahmadMu
-    password=passw0rd
-    dremioserver=http://localhost:9047
+# load environemnt.env
+export $(grep -v '^\s*#.*' ./environment.env | xargs)
 
-    # create_view --name 'my view' --space 'Bronz' --query 'SELECT *'
-    response=$(curl -s -X POST "$dremioserver/apiv2/login" \
-        --header "Content-Type: application/json" \
-        --data-raw "{
-        \"userName\": \"$username\",
-        \"password\": \"$password\"
-        }")
-    echo $response
-    token=$(echo "$response" | grep -oP '"token":\s*"\K[^"]+')
-    echo $token
-    path='BRONZ'
-    name='ccc'
-    query='SELECT 1;'
-    response=$(curl -X POST "$dremioserver/api/v3/catalog" \
-        --header "Authorization: Bearer $token" \
-        --header 'Content-Type: application/json' \
-        --data-raw "{
-        \"entityType\": \"dataset\",
-        \"path\": [
-            \"$path\",
-            \"$name\"
-        ],
-        \"type\": \"VIRTUAL_DATASET\",
-        \"sql\": \"$query\"
-        }")
-    if echo "$response" | grep -q '"error"'; then
-        echo "Error: Failed to create view. Response: $response"
-        return 1
-    else
-        echo "view '$name' created successfully."
-    fi
+# creating nessie data
+docker compose up dremio -d
+
+# create default user
+docker exec -u root dremio /bin/bash -c " \
+echo 'debug:{
+  addDefaultUser: true
+}' >> \
+/opt/dremio/conf/dremio.conf"
+
+# in orderder for new configurations to take effect
+docker restart dremio
+
+sleep 60s
+
+# Now you can login with:
+# username: dremio
+# password: dremio123
+
+# getting credentials
+response=$(curl -s -X POST "$DREMIO_ENDPOINT/apiv2/login" \
+    --header "Content-Type: application/json" \
+    --data-raw "{
+    \"userName\": \"$DREMIO_USERNAME\",
+    \"password\": \"$DREMIO_PASSWORD\"
+    }")
+echo $response
+token=$(echo "$response" | grep -oP '"token":\s*"\K[^"]+')
+echo $token
+
+
+# creating the source
+response=$(curl -X POST "$DREMIO_ENDPOINT/api/v3/catalog" \
+    --header "Authorization: Bearer $token" \
+    --header "Content-Type: application/json" \
+    --data-raw "{
+        \"entityType\": \"source\",
+        \"config\": {
+            \"nessieEndpoint\": \"http://nessie:19120/api/v2\",
+            \"nessieAuthType\": \"NONE\",
+            \"asyncEnabled\": true,
+            \"isCachingEnabled\": true,
+            \"maxCacheSpacePct\": 100,
+            \"credentialType\": \"ACCESS_KEY\",
+            \"awsAccessKey\": \"$MINIO_ACCESS_KEY\",
+            \"awsAccessSecret\": \"$MINIO_SECRET_KEY\",
+            \"awsRootPath\": \"/warehouse\",
+            \"propertyList\": [
+                {
+                    \"name\": \"fs.s3a.path.style.access\",
+                    \"value\": \"true\"
+                },
+                {
+                    \"name\": \"fs.s3a.endpoint\",
+                    \"value\": \"minio:9000\"
+                },
+                {
+                    \"name\": \"dremio.s3.compat\",
+                    \"value\": \"true\"
+                }
+            ],
+            \"secure\": false
+        },
+        \"name\": \"$NESSIE_CATALOG_NAME\",
+        \"type\": \"NESSIE\"
+    }")
+
+echo $response
+
+# creating spaces and views
+python ./Dremio/dremio_init.py
--- start.sh
+++ start.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 set -e 
 
-# Start the first set of services: Spark, Nessie, MinIO, and Dremio
+# Start the first set of services: Spark, Nessie, MinIO, Zookeeper and Kafka
 docker compose up -d \
-  spark \
   nessie \
   minio \
-  mc
-
+  mc \
+  zookeeper \
+  kafka
 
 # write on env file to upate minio end point
+# Making sure that the minio ip has been updated in spark container
 minio_container_name='minio'
 container_id=$(docker container ls -q --filter "name=${minio_container_name}")
 if [ -z "$container_id" ]; then
@@ -21,35 +22,45 @@ container_ip=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddres
 echo "Minio IP: ${container_ip}"
 sed -i "s|^MINIO_END_POINT.*|MINIO_END_POINT=http://${container_ip}:9000|" environment.env
 
+docker compose up -d spark 
+
+# add secrets to login shell
+# this allows executing S3 related spark-submit via SSH
+# step 1: load environment variables
+export $(grep -v '^#' ./environment.env | xargs)
+# step 2: append 'export's in /etc/profile
+docker exec -u root spark /bin/bash -c "\
+echo 'export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY' >> /etc/profile && \
+echo 'export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_KEY' >> /etc/profile && \
+echo 'export AWS_REGION=$AWS_REGION' >> /etc/profile && \
+echo 'export AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION' >> /etc/profile"
+
 #init datalakehouse tables
 docker exec spark spark-submit /includes/python_scripts/data_lakehouse_init.py
 
-# Start Zookeeper and Kafka in a separate command
-docker compose up -d \
-  zookeeper \
-  kafka
-
-sleep 10
-
 # Run Spark Structured Streaming, Data Producer, and MongoDB loader
-timeout 100s python ./includes/python_scripts/data-generators/start_kafka_producer.py &
-timeout 100s docker exec spark spark-submit /includes/ingestion/kafka/load-kafka-stream-transactions.py &
+timeout 120s python ./includes/python_scripts/data-generators/start_kafka_producer.py > producer.log 2>&1 &
+timeout 120s docker exec spark spark-submit /includes/ingestion/kafka/load-kafka-stream-transactions.py > spark_job.log 2>&1 &
 wait
 
-# Shut down Kafka and Zookeeper
-docker compose down \
-  zookeeper \
-  kafka \
-  mc
-
-# # start dremio
-docker compose up -d \
-  dremio
+# shutting down kafka
+# my poor computer can't handle all these containers, meh..
+docker compose down zookeeper kafka
 
 # Initialize and start Airflow
 docker compose -f airflow-compose.yaml up airflow-init
 docker compose -f airflow-compose.yaml up -d
 
 # Connect Spark master to the Airflow network
-docker compose up -d \
-  dremio
+docker network connect airflow-network spark
+
+# configure airflow ssh connector
+# this has to be secret and created manually
+# remember that this is not production setup
+docker exec airflow-webserver /home/airflow/.local/bin/airflow \
+    connections add 'my_ssh_connection' \
+    --conn-type 'ssh' \
+    --conn-login 'me' \
+    --conn-password 'changeme' \
+    --conn-host 'spark' \
+    --conn-port '22'
